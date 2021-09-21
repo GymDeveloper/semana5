6149693d9f4e86112b099cae8de77b0171d8581c
linder3hs <linderhassinger00@gmail.com>
2021-09-21 10:12:43 -0500

    init table view

--- semana5/Main/View/ViewController.swift
+++ semana5/Main/View/ViewController.swift
@@ -7,12 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+// protocolos: UITableViewDelegate, UITableViewDataSource
+// ademas UITableViewDataSource necesita minimo 2 funciones
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
     // Ahora vamos a ver como mostrar la informacion que acabamos de consumir
     
     // Hay que instanciar a nuestro ViewModelHistory
+    @IBOutlet weak var tableView: UITableView!
+    
     var viewModelHistory = ViewModelHistoryPost()
+    
+    // Primero declare mi arreglo de nombres
+    let names: [String] = ["Pepe", "Juan", "Lucas", "Sultano"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +35,10 @@ class ViewController: UIViewController {
     func configureView() {
         // aca ejecuta la funcion que hace el llamado al urlAPI
         viewModelHistory.getDataHistoryPost()
+        // Hice la vista se encargue del delegate y el datasource de mi table
+        // al hacer esto autmaticamente me importo 2 protocolos
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     func printData() {
@@ -46,6 +57,30 @@ class ViewController: UIViewController {
                 self?.printData()
             }
         }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    // por default UITableViewDataSource este necesita estas 2 funciones
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // ? numberOfRowsInSection
+        // iba el count
+        return names.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // cellForRowAt
+        // aca seteo la informacion
+        // Busco a la celda con el id cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+
+        // aca seteo la informacion dando el valor del arreglo como texto
+        cell.textLabel?.text = names[indexPath.row]
+
+        // retorno la lista
+        return cell
     }
 
 }
